6fcc6b64ab782571249d8c53a4b29c19a056deda
Sotiris Malassiotis <malasiot@iti.gr>
2022-12-09 16:29:52 +0200

    kinematic

--- include/xsim/kinematic.hpp
+++ include/xsim/kinematic.hpp
@@ -3,45 +3,116 @@
 
 #include <memory>
 
-#include <bullet/btBulletCollisionCommon.h>
-#include <bullet/btBulletDynamicsCommon.h>
-
-#include <xsim/collision.hpp>
-#include <xsim/convert.hpp>
 #include <xviz/robot/urdf_robot.hpp>
-#include <xsim/rigid_body.hpp>
 
 namespace xsim {
 
-class KinematicCollisionShape: public GroupCollisionShape {
+using JointState = std::map<std::string, double> ;
+
+class KinematicLink ;
+
+using KinematicLinkPtr = std::shared_ptr<KinematicLink> ;
+
+class KinematicJoint {
+public:
+    enum JointType { REVOLUTE_JOINT, PRISMATIC_JOINT, FIXED_JOINT, CONTINUOUS_JOINT };
+    enum LimitCheckType {  LIMIT_CHECK_THROW_EXCEPTION, LIMIT_CHECK_CLAMP_VALUE, LIMIT_CHECK_IGNORE } ;
+
+    KinematicJoint() = default ;
+    virtual ~KinematicJoint() {}
+
+    // get the value corresponding to a dof of this joint
+    virtual double getPosition() const = 0;
+    // set the value corresponding to a dof of this joint
+    virtual void setPosition(double val, LimitCheckType lt = LIMIT_CHECK_CLAMP_VALUE) = 0;
+    // chack if this value is within the allowable limits of the joint
+    virtual bool allows(double value) const { return true ; }
+    // update joint transform matrix based on set values
+    virtual void updateLocalTransform() = 0;
+
+    virtual void getLimits(double &l, double &u) const = 0 ;
+    // compute the distance between dofs
+    virtual double getDistance(double val1, double val2) const = 0 ;
+
+    virtual bool hasDOF() const { return true ; }
+
+    virtual JointType getType() const = 0 ;
+
+    const std::string &name() const { return name_ ; }
+    const KinematicLinkPtr parentLink() const { return parent_ ; }
+    const KinematicLinkPtr childLink() const { return child_ ; }
+    const Eigen::Isometry3f &origin() const { return origin_ ; }
+    const Eigen::Isometry3f &local() const { return local_ ; }
+
+protected:
+    friend class KinematicModel ;
+    friend class KinematicLink ;
+
+    void updateTransforms() ;
+    void updateLinkTransforms() ;
+
+    std::string name_ ;
+    KinematicLinkPtr parent_, child_ ;
+
+    Eigen::Isometry3f origin_ ; // joint frame transform
+    Eigen::Isometry3f local_;  // dof transform
+};
+
+using KinematicJointPtr = std::shared_ptr<KinematicJoint> ;
+
+class KinematicLink {
+public:
+    KinematicLink() = default ;
+
+protected:
+
+    friend class KinematicModel ;
+    friend class KinematicJoint ;
+
+    void updateWorldTransform();
+
+    std::string name_ ;
+    KinematicJointPtr parent_joint_ ;
+    std::vector<KinematicJointPtr> child_joints_ ;
+    Eigen::Isometry3f world_ = Eigen::Isometry3f::Identity(); // world transform
+};
+
+
+class KinematicModel {
 public :
 
-    KinematicCollisionShape(const xviz::URDFRobot &robot) {
+    KinematicModel(const xviz::URDFRobot &robot) {
         create(robot) ;
     }
 
+    // set world transform of root link
+    void setWorldTransform(const Eigen::Isometry3f &tr);
+
+    KinematicJointPtr getJoint(const std::string &name) const;
+    KinematicLinkPtr getLink(const std::string &name) const;
+
+    double getJointPosition(const std::string &name) const;
+    void setJointPosition(const std::string &name, double val, KinematicJoint::LimitCheckType type = KinematicJoint::LIMIT_CHECK_CLAMP_VALUE);
+
+    JointState getJointState() const;
+    JointState getJointState(const std::vector<std::string> &joints) const;
+    void setJointState(const JointState &state, KinematicJoint::LimitCheckType lt = KinematicJoint::LIMIT_CHECK_CLAMP_VALUE);
+
+    Eigen::Isometry3f getLinkTransform(const std::string &link_name) const;
+    std::map<std::string, Eigen::Isometry3f> getLinkTransforms() const;
+
+    // get names of DOF joints
+    std::vector<std::string> getJointNames() const ;
+
 private:
 
-    struct Link {
-        uint col_shape_index_ ;
-        Eigen::Isometry3f origin_ ;
-        CollisionShapePtr shape_ ;
-    };
-
-    struct Joint {
-        Eigen::Isometry3f origin_ ;
-        Eigen::Vector3f axis_ ;
-        float lower_limit_, upper_limit_ ;
-        std::string type_, mimic_ ;
-        Link *parent_, *child_ ;
-        float position_ ;
-    };
-
-    std::map<std::string, Link> links_ ;
-    std::map<std::string, Joint> joints_ ;
+    std::map<std::string, KinematicLinkPtr> links_ ;
+    std::map<std::string, KinematicJointPtr> joints_ ;
+    KinematicLinkPtr root_ ;
 
     void create(const xviz::URDFRobot &robot) ;
-    static CollisionShapePtr makeCollisionShape(const xviz::URDFGeometry *geom) ;
+    void updateWorldTransforms() ;
+
 };
 
 } // namespace xsim
--- src/kinematic.cpp
+++ src/kinematic.cpp
@@ -6,111 +6,362 @@ using namespace xviz ;
 
 namespace xsim {
 
-void KinematicCollisionShape::create(const URDFRobot &robot) {
+class KRevoluteJoint: public KinematicJoint
+{
+public:
+    KRevoluteJoint(const Vector3f &axis, double lower, double upper):
+        lower_(lower), upper_(upper), axis_(axis), position_(0.0)   {
+        local_ = Isometry3f::Identity() ;
+    }
 
-    uint count = 0 ;
+    double getPosition() const override {
+        return position_ ;
+    }
+
+    void getLimits(double &lower, double &upper) const override {
+        lower = lower_ ;
+        upper = upper_ ;
+    }
+
+    void setPosition(double val, KinematicJoint::LimitCheckType lt)  {
+        if ( lt == KinematicJoint::LIMIT_CHECK_CLAMP_VALUE )  {
+            if ( val < lower_ ) position_ = lower_ ;
+            else if ( val > upper_ ) position_ = upper_ ;
+            else position_ = val ;
+        }
+        else if ( lt == KinematicJoint::LIMIT_CHECK_THROW_EXCEPTION ) {
+            if ( val < lower_ || val > upper_  )
+                throw std::runtime_error("DOF out of range") ;
+            else position_ = val ;
+        }
+        else {
+            if ( val >= lower_ && val <= upper_ ) position_ = val ;
+        }
+    }
+
+    double getDistance(double v1, double v2) const override {
+        return fabs(v1 - v2) ;
+    }
+
+    bool allows(double val) const override { return fabs(val) < M_PI && val >= lower_ && val <= upper_ ; }
+
+
+    void updateLocalTransform() override {
+        local_.setIdentity() ;
+        local_.rotate(AngleAxisf(position_, axis_)) ;
+    }
+
+    KinematicJoint::JointType getType() const override { return REVOLUTE_JOINT ; }
+
+    double lower_, upper_ ;
+    Vector3f axis_ ;
+    double position_ ;
+};
+
+
+class KPrismaticJoint: public KinematicJoint
+{
+public:
+    KPrismaticJoint(const Vector3f &axis, double lower, double upper):
+        lower_(lower), upper_(upper), axis_(axis), position_(0.0)   {
+        local_ = Isometry3f::Identity() ;
+    }
+
+    double getPosition() const override {
+        return position_ ;
+    }
+
+    void getLimits(double &lower, double &upper) const override {
+        lower = lower_ ;
+        upper = upper_ ;
+    }
+
+    void setPosition(double val, KinematicJoint::LimitCheckType lt)  {
+        if ( lt == KinematicJoint::LIMIT_CHECK_CLAMP_VALUE )  {
+            if ( val < lower_ ) position_ = lower_ ;
+            else if ( val > upper_ ) position_ = upper_ ;
+            else position_ = val ;
+        }
+        else if ( lt == KinematicJoint::LIMIT_CHECK_THROW_EXCEPTION ) {
+            if ( val < lower_ || val > upper_  )
+                throw std::runtime_error("DOF out of range") ;
+            else position_ = val ;
+        }
+        else {
+            if ( val >= lower_ && val <= upper_ ) position_ = val ;
+        }
+    }
+
+    double getDistance(double v1, double v2) const override {
+        return fabs(v1 - v2) ;
+    }
+
+    bool allows(double val) const override { return fabs(val) < M_PI && val >= lower_ && val <= upper_ ; }
+
+    void updateLocalTransform() {
+        local_.setIdentity() ;
+        local_.translate(axis_* position_) ;
+    }
+
+    KinematicJoint::JointType getType() const { return PRISMATIC_JOINT ; }
+
+    double lower_, upper_ ;
+    Vector3f axis_ ;
+    double position_ ;
+};
+
+static double normalizeCircularAngle(double theta, double l, double u) {
+    if ( theta < l ) {
+        double range = u - l;
+
+        assert(range > 0) ;
+        theta += range;
+
+        while (theta < l) {
+            theta += range;
+        }
+    }
+    else if ( theta > u ) {
+        double range = u - l ;
+        assert( range > 0 ) ;
+        theta -= range;
+        while (theta > u ) {
+            theta -= range;
+        }
+    }
+    return theta;
+}
+
+static double subtractCircularAngle(double f0, double f1)
+{
+    return normalizeCircularAngle(f0-f1, -M_PI, M_PI);
+}
+
+class KContinuousJoint: public KinematicJoint
+{
+public:
+    KContinuousJoint(const Vector3f &axis):
+        axis_(axis), position_(0.0)   {
+        local_ = Isometry3f::Identity() ;
+    }
+
+    double getPosition() const override {
+        return position_ ;
+    }
+
+    void getLimits(double &lower, double &upper) const override {
+        lower = -M_PI ;
+        upper = M_PI ;
+    }
+
+    void setPosition(double val, KinematicJoint::LimitCheckType lt)  {
+        position_ = val ;
+    }
+
+    double getDistance(double v1, double v2) const override {
+        return subtractCircularAngle(v1, v2) ;
+    }
+
+    void updateLocalTransform() {
+        local_.setIdentity() ;
+        local_.translate(axis_* position_) ;
+    }
+
+    KinematicJoint::JointType getType() const { return CONTINUOUS_JOINT ; }
+
+    Vector3f axis_ ;
+    double position_ ;
+};
+
+class KFixedJoint: public KinematicJoint
+{
+public:
+    KFixedJoint() {
+        local_.setIdentity() ;
+    }
+
+    double getPosition() const override { return 0 ; }
+    void setPosition(double, KinematicJoint::LimitCheckType) override {}
+    void updateLocalTransform() override { }
+    double getDistance(double val1, double val2) const override { return 0.0 ; }
+    void getLimits(double &lower, double &upper) const override {
+        lower =  upper = 0.0 ;
+    }
+    bool hasDOF() const override { return false ; }
+
+    KinematicJoint::JointType getType() const { return FIXED_JOINT ; }
+};
+
+void KinematicModel::create(const URDFRobot &robot) {
+
+
     for( const auto &bp: robot.links_ ) {
         const string &name = bp.first ;
         const URDFLink &link = bp.second ;
 
-        Link cl ;
-
-        vector<CollisionShapePtr> shapes ;
-        vector<Isometry3f> origins ;
-
-        for( const auto &geom: link.collision_geoms_ ) {
-
-            Isometry3f col_origin = geom->origin_ ;
-
-            origins.push_back(col_origin) ;
-
-            CollisionShapePtr cs = makeCollisionShape(geom.get()) ;
-
-            if ( cs ) {
-                shapes.push_back(cs) ;
-            }
-        }
-
-        CollisionShapePtr col_shape ;
-        Isometry3f col_origin = Isometry3f::Identity() ;
-
-        if ( shapes.empty() ) {
-            col_shape.reset(new GroupCollisionShape) ;
-            col_shape->setMargin(0.001);
-        }   else if ( shapes.size() == 1 ) {
-            col_shape = shapes[0] ;
-            col_origin = origins[0] ;
-        } else {
-            GroupCollisionShape *gc = new GroupCollisionShape() ;
-
-            for( uint i=0 ; i<shapes.size() ; i++ ) {
-                gc->addChild(shapes[i], origins[i]) ;
-            }
-            col_shape.reset(gc) ;
-            col_shape->setMargin(0.001);
-        }
-
-
-        addChild(col_shape, col_origin) ;
-        cl.col_shape_index_ = count++ ;
-        cl.origin_ = col_origin ;
-        cl.shape_ = col_shape ;
+        KinematicLinkPtr cl(new KinematicLink);
+        cl->name_ = name ;
         links_.emplace(name, cl) ;
     }
 
-    map<string, string> parent_link_tree ;
+    for( const auto &jpr: robot.joints_ ) {
 
-    for( const auto &jp: robot.joints_ ) {
+        const URDFJoint &j = jpr.second ;
 
-        const URDFJoint &j = jp.second ;
+        KinematicJointPtr jp ;
+        if ( j.type_ == "revolute" ) {
+            jp.reset(new KRevoluteJoint(j.axis_, j.lower_, j.upper_)) ;
+        } else if ( j.type_ == "prismatic" ) {
+            jp.reset(new KPrismaticJoint(j.axis_, j.lower_, j.upper_)) ;
+        } else if ( j.type_ == "continuous" ) {
+            jp.reset(new KContinuousJoint(j.axis_)) ;
+        } else if ( j.type_ == "fixed" ) {
+            jp.reset(new KFixedJoint()) ;
+        }
 
-        Joint joint ;
+        jp->origin_ = j.origin_ ;
+        jp->name_ = j.name_ ;
 
-        joint.origin_ = j.origin_ ;
-        string parent_link_name = j.parent_ ;
-        string child_link_name = j.child_ ;
+        auto it = links_.find(j.parent_) ;
+        if ( it == links_.end() ) continue ;
+        else jp->parent_ = it->second ;
 
-        Link *parent_link, *child_link ; ;
+        it = links_.find(j.child_) ;
+        if ( it == links_.end() ) continue ;
+        else jp->child_ = it->second ;
 
-        auto pl_it = links_.find(parent_link_name) ;
-        if ( pl_it == links_.end() ) continue ;
-        else parent_link = &pl_it->second ;
+        joints_.emplace(j.name_, jp) ;
+    }
 
-        auto cl_it = links_.find(child_link_name) ;
-        if ( cl_it == links_.end() ) continue ;
-        else child_link = &cl_it->second ;
+    for( const auto &jp: joints_ ) {
+        const auto joint = jp.second ;
+        KinematicLinkPtr parent = joint->parent_ ;
+        KinematicLinkPtr child = joint->child_ ;
 
-        parent_link_tree[child_link_name] = parent_link_name ;
+        assert( parent && child ) ;
 
-        joint.type_ = j.type_ ;
-        joint.parent_ = parent_link ;
-        joint.child_ = child_link ;
-        joint.lower_limit_ = j.lower_ ;
-        joint.upper_limit_ = j.upper_ ;
-        joint.axis_ = j.axis_ ;
-        joint.mimic_ = j.mimic_joint_ ;
+        parent->child_joints_.push_back(joint) ;
+        child->parent_joint_ = joint;
+    }
 
-        joints_.emplace(j.name_, joint) ;
+    for( const auto &lp: links_ ) {
+        if ( lp.second->parent_joint_ == nullptr )
+            root_ = lp.second ;
+    }
+
+    updateWorldTransforms();
+}
+
+void KinematicModel::updateWorldTransforms() {
+    root_->updateWorldTransform();
+}
+
+void KinematicLink::updateWorldTransform() {
+    if ( parent_joint_ )
+        world_ = parent_joint_->parentLink()->world_ * parent_joint_->origin() * parent_joint_->local();
+    for( const auto &cl: child_joints_ ) {
+        cl->child_->updateWorldTransform();
     }
 }
 
-CollisionShapePtr KinematicCollisionShape::makeCollisionShape(const URDFGeometry *geom) {
-    float scale = 1.0 ;
-    CollisionShapePtr shape ;
-
-    if ( const URDFBoxGeometry *g = dynamic_cast<const URDFBoxGeometry *>(geom) ) {
-        shape.reset(new BoxCollisionShape(g->he_))  ;
-    } else if ( const URDFCylinderGeometry *g = dynamic_cast<const URDFCylinderGeometry *>(geom) ) {
-        shape.reset(new CylinderCollisionShape(g->radius_, g->height_/2.0, CylinderCollisionShape::ZAxis))  ;
-    } else if ( const URDFMeshGeometry *g = dynamic_cast<const URDFMeshGeometry *>(geom) ) {
-        shape.reset(new ConvexHullCollisionShape(g->path_, g->scale_));
-    } else if ( const URDFSphereGeometry *g = dynamic_cast<const URDFSphereGeometry *>(geom) ) {
-        shape.reset(new SphereCollisionShape(g->radius_));
-    }
-
-    if ( shape ) shape->setLocalScale(scale) ;
-    return shape ;
+void KinematicJoint::updateLinkTransforms() {
+    child_->updateWorldTransform();
 }
 
+void KinematicModel::setWorldTransform(const Isometry3f &tr) {
+    root_->world_ = tr ;
+    updateWorldTransforms();
+}
+
+KinematicJointPtr KinematicModel::getJoint(const std::string &name) const {
+    auto it = joints_.find(name);
+    if ( it == joints_.end() ) return nullptr ;
+    else return (*it).second ;
+}
+
+KinematicLinkPtr KinematicModel::getLink(const std::string &name) const {
+    auto it = links_.find(name) ;
+    if ( it == links_.end() ) return nullptr ;
+    else return (*it).second ;
+}
+
+double KinematicModel::getJointPosition(const std::string &name) const {
+    KinematicJointPtr joint = getJoint(name) ;
+    assert(joint) ;
+    return joint->getPosition() ;
+}
+
+void KinematicModel::setJointPosition(const std::string &name, double val, KinematicJoint::LimitCheckType type) {
+    KinematicJointPtr joint = getJoint(name) ;
+    assert(joint) ;
+
+    joint->setPosition(val, type) ;
+    joint->updateLocalTransform() ;
+    joint->updateLinkTransforms() ;
+}
+
+
+
+JointState KinematicModel::getJointState() const {
+    JointState state ;
+    for( const auto &jp: joints_ ) {
+        auto joint = jp.second ;
+        if ( joint->hasDOF() ) {
+            state.emplace(jp.first, joint->getPosition()) ;
+        }
+    }
+    return state ;
+}
+
+JointState KinematicModel::getJointState(const vector<string> &joints) const {
+    JointState state ;
+    for( const auto &name: joints ) {
+        auto joint = getJoint(name) ;
+        if ( !joint ) continue ;
+        if ( joint->hasDOF() )
+           state.emplace(name, joint->getPosition()) ;
+     }
+    return state ;
+}
+
+void KinematicModel::setJointState(const JointState &state, KinematicJoint::LimitCheckType lt) {
+    for ( const auto &sp: state ) {
+        const string &name = sp.first ;
+        double val = sp.second ;
+
+        auto joint = getJoint(name) ;
+        if ( joint ) {
+            joint->setPosition(val, lt) ;
+            joint->updateLocalTransform();
+        }
+    }
+
+    updateWorldTransforms() ;
+}
+
+Isometry3f KinematicModel::getLinkTransform(const string &link_name) const {
+    auto link = getLink(link_name) ;
+    assert(link) ;
+    return link->world_ ;
+}
+
+map<string, Isometry3f> KinematicModel::getLinkTransforms() const {
+    map<string, Isometry3f> trs ;
+    for( const auto &lp: links_ ) {
+        trs.emplace(lp.first, lp.second->world_) ;
+    }
+    return trs ;
+}
+
+std::vector<string> KinematicModel::getJointNames() const {
+    vector<string> joints ;
+    for( const auto &jp: joints_ ) {
+        auto joint = jp.second ;
+        if ( joint->hasDOF() ) {
+            joints.push_back(jp.first) ;
+        }
+    }
+    return joints ;
+}
 
 }
